3264a291560861160de15944980a6d2e0119bbfa
chris <chris@MacBook-Pro-2.local>
2026-05-20 11:49:51 +0200

    langue sauvguarde

--- src/proxy.ts
+++ src/proxy.ts
@@ -3,7 +3,15 @@ import { NextResponse } from "next/server"
 import type { NextRequest } from "next/server"
 import { defaultLocale, isLocale, locales } from "@/app/i18n/config"
 
+const localeCookieName = "NEXT_LOCALE"
+
 const getPreferredLocale = (request: NextRequest) => {
+    const savedLocale = request.cookies.get(localeCookieName)?.value
+
+    if (savedLocale && isLocale(savedLocale)) {
+        return savedLocale
+    }
+
     const acceptLanguage = request.headers.get("accept-language")
 
     if (!acceptLanguage) {
@@ -16,17 +24,14 @@ const getPreferredLocale = (request: NextRequest) => {
         .filter(Boolean)
 
     for (const language of browserLanguages) {
-        const exactMatch = locales.find((locale) => locale === language)
-
-        if (exactMatch) {
-            return exactMatch
+        if (isLocale(language)) {
+            return language
         }
 
         const baseLanguage = language.split("-")[0]
-        const baseMatch = locales.find((locale) => locale === baseLanguage)
 
-        if (baseMatch) {
-            return baseMatch
+        if (isLocale(baseLanguage)) {
+            return baseLanguage
         }
     }
 
@@ -48,13 +53,17 @@ export const proxy = (request: NextRequest) => {
     const maybeLocale = segments[1]
 
     if (isLocale(maybeLocale)) {
-        return NextResponse.next()
+        const response = NextResponse.next()
+
+        response.cookies.set(localeCookieName, maybeLocale, {
+            path: "/",
+            maxAge: 60 * 60 * 24 * 365
+        })
+
+        return response
     }
 
-    const locale =
-        pathname === "/"
-            ? getPreferredLocale(request)
-            : defaultLocale
+    const locale = getPreferredLocale(request)
 
     const url = request.nextUrl.clone()
     url.pathname = `/${locale}${pathname}`
